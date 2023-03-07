f0d6e094c8b323fb26cd9995964bcf44efbc8a33
Benisekjan <janik.benisek@seznam.cz>
2023-03-07 10:17:46 +0100

    04 - formular v HTML

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 /* Připojení modulu frameworku Express */
 const express = require("express");
 /* Vytvoření základního objektu serverové aplikace */
+const bodyParser = require("body-parser");
+/* Připojení externího modulu moment (https://momentjs.com/) - knihovna pro formátování
+datových a časových údajů */
+const moment = require("moment");
+/* Připojení vestavěných modulů fs (práce se soubory) a path (cesty v adresářové struktuře) */
+const fs = require("fs");
+const path = require("path");
 const app = express();
 /* Nastavení portu, na němž bude spuštěný server naslouchat */
 const port = 3000;
